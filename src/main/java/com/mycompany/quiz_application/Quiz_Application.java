34afad1680ea3bc5f59bcabb5a8d434bc954ffad
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2024-05-05 14:55:57 -0400

    finsihed as of 05-05-2024 release me

---
+++ src/main/java/com/mycompany/quiz_application/Quiz_Application.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quiz_application;
+
+/**
+ *
+ * @author lmuen
+ */
+public class Quiz_Application {
+
+    public static void main(String[] args) {
+        new Start().setVisible(true);
+    }
+}
